f8d11b23c8c71f66fb7ce89aef17e4cc2ba996b9
Aarni Koskela <aarni.koskela@andersinno.fi>
2014-10-17 13:59:23 +0300

    Smarten `go_back`

--- gitkit/branches.py
+++ gitkit/branches.py
@@ -22,10 +22,16 @@ def del_merged(ref):
 
 def go_back():
 	current_branch = get_output("git rev-parse --abbrev-ref HEAD").strip()
-	reflog_entry = get_output(["git", "log", "-1", "-g", "--pretty=format:%H:%ar:%gs", "--grep-reflog=moving from .* to %s" % current_branch])
-	last_other_branch = re.search("from (.+?) to ", reflog_entry).group(1)
-	if yorn("Checkout %s?" % last_other_branch):
-		run(["git", "checkout", last_other_branch])
+	reflog_entries = get_lines(["git", "log", "-g", "--pretty=format:%H:%ar:%gs", "--grep-reflog=moving from .* to %s" % current_branch])
+	for reflog_entry in reflog_entries:
+		branch_match = re.search("from (.+?) to (.+?)$", reflog_entry)
+		prev_branch = branch_match.group(1)
+		new_branch = branch_match.group(2)
+
+		if prev_branch != new_branch:
+			if yorn("Checkout %s?" % prev_branch):
+				run(["git", "checkout", prev_branch])
+				break
 
 
 def install(cli):
