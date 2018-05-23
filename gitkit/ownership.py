061f4b8e1b649079959611136b17bcc958bb71ba
Aarni Koskela <akx@iki.fi>
2018-05-23 15:03:19 +0300

    Embetter `ownership` tool

--- gitkit/ownership.py
+++ gitkit/ownership.py
@@ -7,14 +7,25 @@ from collections import Counter
 
 import click
 
-from gitkit.util import get_lines
+from gitkit.util import get_lines, get_output
 
-INVALID_RE = re.compile("(jpg|jpeg|css|png|gif|dat|ds_store|ttf|woff|eot|svg|psd)$", re.I)
+INVALID_EXTENSIONS = {
+    'css',
+    'dat',
+    'ds_store',
+    'eot',
+    'gif',
+    'jpeg',
+    'png',
+    'psd',
+    'svg',
+    'ttf',
+    'woff',
+    'jpg',
+    'deb',
+}
 
-try:
-    text_type = unicode
-except NameError:
-    text_type = str
+INVALID_RE = re.compile("(%s)$" % '|'.join(INVALID_EXTENSIONS), re.I)
 
 
 class OwnershipMachine(object):
@@ -23,7 +34,7 @@ class OwnershipMachine(object):
         self.blob_blame_cache = {}
 
     def is_ownable_type(self, path):
-        path = text_type(path).lower()
+        path = str(path).lower()
         if INVALID_RE.search(path):
             return False
         if "/migrations/" in path:
@@ -34,12 +45,15 @@ class OwnershipMachine(object):
 
     def blame_lines(self, commit, path):
         lines_by_author = Counter()
-        for line in get_lines(["git", "blame", "-w", "-M", "-C", "--line-porcelain", commit, "--", path]):
+        for line in get_lines(
+            ["git", "blame", "-w", "-M", "-C", "--line-porcelain", commit, "--", path],
+            strip_left=False,
+        ):
             if line.startswith("author "):
                 lines_by_author[line.split(" ", 1)[1]] += 1
         return dict(lines_by_author)
 
-    def process_commit(self, last_commit, commit):
+    def process_commit(self, commit, progress=False):
         paths = {}
         lines_by_author = Counter()
         for line in get_lines(["git", "ls-tree", "-r", commit]):
@@ -54,19 +68,25 @@ class OwnershipMachine(object):
         print("%s: %s files" % (commit, len(paths)), file=sys.stderr)
 
         blob_blames = {}
-        for path, sha in paths.items():
-            blob_blame = self.blob_blame_cache.get(sha)
-            if not blob_blame:
-                blob_blame = self.blame_lines(commit, path)
-                self.blob_blame_cache[sha] = blob_blame
-            blob_blames[sha] = blob_blame
+        path_sha_iter = click.progressbar(
+            list(paths.items()),
+            item_show_func=lambda i: (i[0] if i else None),
+        )
+        path_sha_iter.is_hidden = (not progress)
+        with path_sha_iter:
+            for path, sha in path_sha_iter:
+                blob_blame = self.blob_blame_cache.get(sha)
+                if not blob_blame:
+                    blob_blame = self.blame_lines(commit, path)
+                    self.blob_blame_cache[sha] = blob_blame
+                blob_blames[sha] = blob_blame
 
         for blob_blame in list(blob_blames.values()):
             for author, n_lines in blob_blame.items():
                 lines_by_author[str(author)] += n_lines
         return lines_by_author
 
-    def calculate(self, ref):
+    def calculate_over_time(self, ref, step=5):
         commits = {}
         for line in get_lines(["git", "log", "--pretty=format:%at %H", ref]):
             time_str, hash = line.split()
@@ -75,25 +95,33 @@ class OwnershipMachine(object):
 
         print("Gathered", len(commits), "days of history")
 
-        lines_by_author_per_day = {}
-
-        last_commit = None
         for i, (date, commit) in enumerate(sorted(commits.items())):
-            if i % 5:
+            if i % step:
                 continue
             print("Processing commit %d / %d..." % (i, len(commits)), file=sys.stderr)
-            lines_by_author = self.process_commit(last_commit, commit)
-            lines_by_author_per_day[date] = lines_by_author
-            print(json.dumps({
+            lines_by_author = self.process_commit(commit)
+            day_datum = {
                 "date": date,
                 "commit": str(commit),
                 "owners": dict(lines_by_author),
                 "total": sum(lines_by_author.values())
-            }))
-            last_commit = commit
+            }
+            yield day_datum
 
 
 @click.command()
-def ownership(ref="master"):
+@click.option('--step', type=int, default=5, )
+@click.option('--over-time/--no-over-time', default=False)
+def ownership(ref="master", step=5, over_time=False):
     om = OwnershipMachine()
-    om.calculate(ref)
+    if over_time:
+        for datum in om.calculate_over_time(ref, step=step):
+            print(json.dumps(datum))
+    else:
+        commit = get_output('git rev-parse %s' % ref)
+        print('Processing commit %s...' % commit, file=sys.stderr)
+        lines_by_author = om.process_commit(commit, progress=True)
+        print(json.dumps({
+            "owners": dict(lines_by_author),
+            "total": sum(lines_by_author.values()),
+        }))
